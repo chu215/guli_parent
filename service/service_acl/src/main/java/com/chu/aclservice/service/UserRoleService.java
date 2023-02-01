1b3e9b578abeb179cd363d0e27a77200a3b9df95
chu <2254886767@qq.com>
2023-02-01 23:16:30 +0800

    实现权限管理功能

---
+++ service/service_acl/src/main/java/com/chu/aclservice/service/UserRoleService.java
@@ -0,0 +1,16 @@
+package com.chu.aclservice.service;
+
+import com.chu.aclservice.entity.UserRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+public interface UserRoleService extends IService<UserRole> {
+
+}
